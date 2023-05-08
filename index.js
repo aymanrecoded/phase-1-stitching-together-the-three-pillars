b0953b9c33b33210c00470c7a31ea28fd146f816
aymanattar <aymanxman19999@gmail.com>
2023-05-08 08:32:09 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 /* This is a file inserted to keep the learn IDE browser happy */
 const testVar = {}
+const articleHearts = document.querySelectorAll(".like-glyph");
+const glyphStates = {
+    "♡": "♥",
+    "♥": "♡"
+};
+const colorStates = {
+    "red": "",
+    "": "red"
+};
+const serverMessage = "Thank you for the like!";
+
+function likeCallback(e) {
+    const heart = e.target;
+    mimicServerCall()
+        .then(function (serverMessage) {
+            alert("You notified the server!");
+            heart.innerText = glyphStates[heart.innerText];
+            heart.style.color = colorStates[heart.style.color];
+        })
+        .catch(function (error) {
+            alert("Something went wrong!");
+        });
+}
+
+for (const glyph of articleHearts) {
+    glyph.addEventListener("click", likeCallback);
+}
